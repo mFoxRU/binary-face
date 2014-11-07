098c6f61bc10a9a65269a8e7c3578b3690f9344e
mFoxRU <mfoxru@gmail.com>
2014-11-07 04:24:16 +0300

    Added: Template loading

---
+++ binaryface/imaging.py
@@ -0,0 +1,29 @@
+__author__ = 'mFoxRU'
+
+import os
+
+from configobj import ConfigObj
+
+
+def _load_template_config(template_name):
+    template_filename = os.path.join(
+        os.curdir,
+        'template',
+        template_name,
+        'config.ini'
+    )
+    try:
+        config = ConfigObj(template_filename)
+    except Exception as e:
+        exit(e)
+    else:
+        #ToDo: Add validation
+        return config
+
+
+def load_template(template_name=None):
+    if template_name is None:
+        template_name = 'default'
+    template = _load_template_config(template_name)
+
+
