d3b3a5a5debeac7a6f1b5f6b44c32020be91d83c
mFoxRU <mfoxru@gmail.com>
2014-11-09 11:47:01 +0300

    Improved: ImageSet and ImageItem basic functionality and documentation

--- binaryface/imaging.py
+++ binaryface/imaging.py
@@ -5,24 +5,22 @@ from PIL import Image
 
 class ImageItem(object):
 
-    def __init__(self, image_file, align=None, align_point=None, offset=None):
+    def __init__(self, image, align=None, offset=None):
         """
         This class represents individual images stored in ImageSet
-        :param image_file: Image file
+        :param image: Image file
         Following image parameters are optional. They override parent values.
         In case of absence parent ImageSet values will be used. Useful when
         images in one imageset have different size
-        :param align:
-        :param align_point:
-        :param offset: tuple of (x, y)
+        :param align: See ImageSet description
+        :param offset: See ImageSet description
         """
         try:
-            self.image = Image.open(image_file)
+            self.image = Image.open(image)
         except Exception as e:
             exit('Could not load file "{0}". {1}'.
-                 format(image_file, e))
+                 format(image, e))
         self.align = align
-        self.align_point = align_point
         self.offset = offset
 
 
@@ -30,14 +28,34 @@ class ImageSet(object):
     imagesets = {}
     attributes = []
 
-    def __init__(self, name, parent=None, attribute=None):
+    def __init__(self, name, imagesets, attribute=None, parent=None,
+                 align=None, offset=None):
         """
         This class contains a set of images that visualises selected attribute.
         :param name: Images set name
+        :param imagesets: Dictionary of images, where keys are attribute values
+        (i.e. 0, 1) and values are dictionaries of ImageItem __init__
+        parameters
+        :param attribute: Attribute that is visualised by the current image set
         :param parent: Images set parent name. This parameter is used for
         image positioning. If parent is None then image will be positioned
-        to the top left corner
-        :param attribute: Attribute that is visualised by the current image set
+        against the top left corner
+        If parent is not None, the following parameters will be used to
+        position image:
+        :param align: Determines align point of the parent image. 1 is top left
+        pixel (0,0), 2 is top middle pixel, 5 is center, etc. See table for
+        reference. When calculating points 2, 4, 5, 6, 8 values a rounded
+        down (i.e. 100.5 -> 100)
+        :param offset: tuple (X, Y) of int representing offset of top left
+        point of image from the parent image align point. Can be negative
+        Table:
+            1---2---3------> X
+            |       |
+            4   5   6
+            |       |
+            7---8---9
+            |
+            v Y
         """
         if name in self.imagesets:
             exit('Name conflict. Image set "{}" already exists'.format(name))
@@ -52,5 +70,9 @@ class ImageSet(object):
                 self.attribute = 0
         if self.attribute not in self.attributes:
             self.attributes.append(self.attribute)
-
+        self.align = align
+        self.offset = offset
+        self.images = {}
+        for image_name, params in imagesets.iteritems():
+            self.images[image_name] = ImageItem(**params)
         self.imagesets[name] = self
