a026c490d08ce34fcb6f99e743990bf860852a65
mFoxRU <mfoxru@gmail.com>
2014-11-09 12:03:44 +0300

    Added: Documentation to load_template() Mix: Minor refactoring

--- binaryface/templating.py
+++ binaryface/templating.py
@@ -7,15 +7,20 @@ from configobj import ConfigObj
 from imaging import ImageSet
 
 
-def load_template(template_name=None):
-    if template_name is None:
-        template_name = 'default'
+def load_template(template_name='default'):
+    """
+    Loads template
+    :param template_name: Template name. It's the same as template folder name
+    in ./templates folder. Template folder must contain config.ini file in it
+    If no name is provided the default template will be used
+    """
     template_dir = os.path.join(os.curdir, 'templates', template_name)
     config_filename = os.path.join(template_dir, 'config.ini')
     try:
         template = ConfigObj(config_filename, file_error=True)
     except Exception as e:
-        exit(e)
+        exit('Could not read config file "{0}". {1}'.format(config_filename,
+                                                            e))
     else:
         #ToDo: Add validation
         for set_name in template.sections:
