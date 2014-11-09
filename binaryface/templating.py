eb87ca78db2a184662e3106ea3001fb61a35efc4
mFoxRU <mfoxru@gmail.com>
2014-11-09 11:49:58 +0300

    Changed: Merged _read_template_config() in load_template Added: Finished template loading

--- binaryface/templating.py
+++ binaryface/templating.py
@@ -4,25 +4,27 @@ import os
 
 from configobj import ConfigObj
 
-
-def _read_template_config(template_name):
-    template_filename = os.path.join(
-        os.curdir,
-        'templates',
-        template_name,
-        'config.ini'
-    )
-    try:
-        config = ConfigObj(template_filename, file_error=True)
-    except Exception as e:
-        exit(e)
-    else:
-        #ToDo: Add validation
-        return config
+from imaging import ImageSet
 
 
 def load_template(template_name=None):
     if template_name is None:
         template_name = 'default'
-    template = _read_template_config(template_name)
-
+    template_dir = os.path.join(os.curdir, 'templates', template_name)
+    config_filename = os.path.join(template_dir, 'config.ini')
+    try:
+        template = ConfigObj(config_filename, file_error=True)
+    except Exception as e:
+        exit(e)
+    else:
+        #ToDo: Add validation
+        for set_name in template.sections:
+            imagesets, params = {}, {}
+            for k, v in template[set_name].items():
+                if isinstance(v, dict):
+                    imagesets[k] = v
+                    imagesets[k]['image'] = os.path.join(template_dir,
+                                                         imagesets[k]['image'])
+                else:
+                    params[k] = v
+            ImageSet(set_name, imagesets, **params)
