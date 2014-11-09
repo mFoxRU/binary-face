197dcaae00224fec48cf3205013787972c2c5d30
mFoxRU <mfoxru@gmail.com>
2014-11-09 07:27:09 +0300

    Added: ImageItem class prototype

--- binaryface/imaging.py
+++ binaryface/imaging.py
@@ -1,5 +1,30 @@
 __author__ = 'mFoxRU'
 
+from PIL import Image
+
+
+class ImageItem(object):
+
+    def __init__(self, image_file, align=None, align_point=None, offset=None):
+        """
+        This class represents individual images stored in ImageSet
+        :param image_file: Image file
+        Following image parameters are optional. They override parent values.
+        In case of absence parent ImageSet values will be used. Useful when
+        images in one imageset have different size
+        :param align:
+        :param align_point:
+        :param offset: tuple of (x, y)
+        """
+        try:
+            self.image = Image.open(image_file)
+        except Exception as e:
+            exit('Could not load file "{0}". {1}'.
+                 format(image_file, e))
+        self.align = align
+        self.align_point = align_point
+        self.offset = offset
+
 
 class ImageSet(object):
     imagesets = {}
@@ -7,7 +32,7 @@ class ImageSet(object):
 
     def __init__(self, name, parent=None, attribute=None):
         """
-        ImageSet represents a set of images that visualises selected attribute.
+        This class contains a set of images that visualises selected attribute.
         :param name: Images set name
         :param parent: Images set parent name. This parameter is used for
         image positioning. If parent is None then image will be positioned
